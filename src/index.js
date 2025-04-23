75e721466cff584dcdebd23fa789358e6fcab68e
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2025-04-23 12:52:54 -0700

    setting up routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Add /profile:1 to the end of the url to see the profile');
+    res.send('Add /profile/1 to the end of the url to see the profile');
 });
 
-app.get('/profile:1', (req, res) => {
-    res.send('Profile 1');
+app.get('/profile/1', (req, res) => {
+    res.send('Profile 1, now try /profile/2');
+});
+
+app.get('/profile/2', (req, res) => {
+    res.send('Profile 2, now try /profile/3');
+});
+
+app.get('/profile/3', (req, res) => {
+    res.send('Profile 3');
 });
 
 app.listen(3000, () => {
