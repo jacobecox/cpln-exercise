6d32f2b48c3898857b07f4419c72314b093cb690
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2025-04-23 12:15:16 -0700

    setting up routes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Add /profile:1 to the end of the url to see the profile');
+});
+
+app.get('/profile:1', (req, res) => {
+    res.send('Profile 1');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is listening on port 3000');
 });
